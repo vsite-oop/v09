279a07c98ca63d30c154bc664d4dfa01f2280e32
Deni Kiseljak <dekiseljak@vsite.hr>
2023-01-09 19:57:40 +0100

    initial commit

--- app/app.cpp
+++ app/app.cpp
@@ -3,4 +3,7 @@
 namespace vsite::oop::v9
 {
 
+	std::vector<int> fill_vector(std::vector<int> v, int num) {
+		
+	}
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <vector>
 
 namespace vsite::oop::v9 
 {
+	std::vector<int> fill_vector(std::vector<int> v, int num);
 }
