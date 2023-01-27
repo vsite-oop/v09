5a335758e2cb3f45161b1e4cc5c378d16cb4ee33
Filip Lončar <filoncar@vsite.hr>
2023-01-27 20:47:43 +0100

    app.h

--- app/app.h
+++ app/app.h
@@ -1,5 +1,34 @@
 #pragma once
 
+#include <vector>
+#include <list>
+#include<map>
+
+#include<string>
+
 namespace vsite::oop::v9 
 {
+	void fill_vector(std::vector<int>& v, int i);
+
+	void remove_element(std::vector<int>& v, int i);
+
+	void input_element(std::vector<std::string>& v, int p, const std::string val);
+
+
+	int list_nth_element(std::list<int> l, int i);
+
+	void list_sort_desc(std::list<int>& l);
+
+	unsigned int unique_numbers(std::istream& is);
+
+
+	class word_frequency {
+	private:
+		std::map<std::string, unsigned int> m;
+	public:
+		word_frequency(std::istream& is);
+
+		unsigned int count() const;
+		unsigned int frequency(std::string s) const;
+	};
 }
