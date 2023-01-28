dc21f9a34b6dd8597b5849c1b367ed1a0017e4c8
Matko <tkoma999@gmail.com>
2023-01-28 19:30:31 +0100

    V09.1

--- app/app.cpp
+++ app/app.cpp
@@ -1,33 +1,65 @@
-#pragma once
+#include "app.h"
 
-#include <vector>
-#include <string>
-#include <list>
-#include <iostream>
-#include <set>
-#include <map>
-
-
-namespace vsite::oop::v9 
+namespace vsite::oop::v9
 {
-   void fill_vector(std::vector<int>& v,int size);
-   void remove_element(std::vector<int>& v,int i);
-   void input_element(std::vector<std::string>& v,int i,std::string s);
-   int list_nth_element(std::list<int>& l, int n);
-	void list_sort_desc(std::list<int>& l);
-	unsigned unique_numbers(std::istream& ss);
+    void fill_vector(std::vector<int>& v,int size){
+        for (int i = 0; i < size; ++i) {
+			v.push_back(i * i);
+		}
+    }
 
-	class word_frequency {
+    void remove_element(std::vector<int>& v,int i){
+        v.erase(v.begin() + i);
+    }
 
-		std::map<std::string, unsigned> m;
-		void insert(const std::string& s);
+    void input_element(std::vector<std::string>& v,int i,std::string s){
+           v.insert(v.begin() + i, s);
 
-	  public:
+    }
 
-		word_frequency(std::istream& ss);
-		unsigned count()const;
-		unsigned frequency(std::string s)const;
+    int list_nth_element(std::list<int>& l, int n){
+            auto it = l.begin();
+		    for (int i = 0; i < n; ++i)++it;
+		    return *it;
+    }
+    
+    void list_sort_desc(std::list<int>& l){
 
-	};
+        l.sort(std::greater<int>());
+    }
+
+    unsigned unique_numbers(std::istream& ss){
+
+        std::set<int> x{};
+		int a;
+		while (ss) {
+			ss >> a;
+			x.insert(a);
+		}
+		return x.size();
+    }
+    word_frequency::word_frequency(std::istream& ss) {
+		std::string str;
+		while (ss) {
+			ss >> str;
+			str[0] = tolower(str[0]);
+			insert(str);
+		}
+	}
+
+	void word_frequency::insert(const std::string& ss) {
+		++m[ss];
+	}
+
+	unsigned word_frequency::count()const {
+		return m.size();
+	}
+
+	unsigned word_frequency::frequency(std::string s)const {
+		auto it = m.find(s);
+		return it != m.end() ? it->second : 0;
+	}
+
+
+
 }
-
