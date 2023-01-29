709f5af80666f45407ed2233b1966c9c99e2e3cb
Ivan Selendic <ivan.selendic@gmail.com>
2023-01-29 22:54:02 +0100

    Tests passed

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,67 @@
 
 namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& v, int size) {
+		for (int i = 0; i < size; i++) {
+			v.push_back(i * i);
+		}
+
+
+	}
+	void remove_element(std::vector<int>& v, int position) {
+		v.erase(v.begin() + position);
+
+	}
+
+	void input_element(std::vector<std::string>& v, int  position, std::string element) {
+		v.insert(v.begin() + position, element);
+
+	}
+
+	int list_nth_element(std::list<int>& l, int position) {
+
+		auto it = l.begin();
+
+		std::advance(it, position);
+
+		return *it;
+
+	}
+
+
+	void list_sort_desc(std::list<int>& l) {
+		l.sort(); l.reverse();
+	}
+
+	unsigned int unique_numbers(std::stringstream& ss) {
+		std::set<int> s{};
+		int a;
+		while (ss >> a) {
+			s.insert(a);
+		}
+		return s.size();
+	}
+
+	word_frequency::word_frequency(std::stringstream& ss) {
+		std::string str;
+		while (ss >> str) {
+
+			str[0] = tolower(str[0]);
+			insert(str);
+		}
+	}
+
+	void word_frequency::insert(const std::string& str) {
+		++m[str];
+	}
+
+	unsigned int word_frequency::count()const {
+		return m.size();
+	}
+
+	unsigned int word_frequency::frequency(std::string str)const {
+		auto it = m.find(str);
+		return it != m.end() ? it->second : 0;
+	}
 
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,29 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+#include <set>
+#include <map>
+#include<sstream>
 
-namespace vsite::oop::v9 
+
+namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& v, int size);
+	void remove_element(std::vector<int>& v, int position);
+	void input_element(std::vector<std::string>& v, int  position, std::string element);
+	int list_nth_element(std::list<int>& l, int position);
+	void list_sort_desc(std::list<int>& l);
+	unsigned int unique_numbers(std::stringstream& ss);
+
+	class word_frequency {
+
+		std::map<std::string, unsigned int> m;
+		void insert(const std::string& str);
+	public:
+		word_frequency(std::stringstream& ss);
+		unsigned int count() const;
+		unsigned int frequency(std::string str)const;
+	};
 }
