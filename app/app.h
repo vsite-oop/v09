6b6f92edc45ae0e981bfff1ceabba3b9f33af722
Tin Bašić <tin.basic22@gmail.com>
2024-01-14 17:49:00 +0100

    Lab09

--- app/app.h
+++ app/app.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
 
-namespace vsite::oop::v9 
+namespace vsite::oop::v9
 {
+    void fill_vector(std::vector<int>& v, size_t n);
+    void remove_element(std::vector<int>& v, size_t n);
+    void input_element(std::vector<std::string>& v, size_t n, std::string s);
+    int list_nth_element(std::list<int>& l, size_t n);
+    void list_sort_desc(std::list<int>& l);
+    unsigned int unique_numbers(std::istream& is);
+    void toLowerCase(std::string& s);
+
+    class word_frequency
+    {
+    private:
+        std::map<std::string, unsigned int> _dict;
+
+    public:
+        word_frequency(std::istream& is);
+
+        unsigned int count() const;
+        unsigned int frequency(const std::string& word) const;
+    };
 }
