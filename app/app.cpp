8af3b596a63d3f3278497ad8fa64e7da5325294e
Mihael Krpes <mikrpes@vsite.hr>
2024-01-16 15:56:27 +0100

    v09

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,50 @@
 
 namespace vsite::oop::v9
 {
+	void fill_vector(std::vector<int>& v, int len) {
+		for (int i = 0; i < len; i++) {
+			v.push_back(i * i);
+		}
+	}
+	void remove_element(std::vector<int>& v, int n) {
+		v.erase(v.begin() + n);
+	}
+	void input_element(std::vector<std::string>& s, int n, std::string str) {
+		s.insert(s.begin() + n, str);
+	}
+	int list_nth_element(std::list<int>& l, int n) {
+		std::list<int>::iterator i= l.begin();
+		std::advance(i, n);
+		return *i;
+	}
+	void list_sort_desc(std::list<int>& l) {
+		l.sort(std::greater<int>());
+	}
+	unsigned int unique_numbers(std::istream& is) {
+		std::unordered_set<int> nums;
+		int v;
+		while (is >> v) {
+			nums.insert(v);
+		}
+		return nums.size();
+	}
+	
+	word_frequency::word_frequency(std::stringstream& s){
+		std::string word;
+		while (s >> word) {
+			for (char& c : word) {
+				c=std::tolower(c);
+			}
+			map[word]++;
+		}
+	}
 
+	unsigned int word_frequency::count() const{
+		return map.size();
+	}
+
+	unsigned int word_frequency::frequency(std::string s) const {
+		auto it = map.find(s);
+		return (it!=map.end()) ? it->second : 0;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,27 @@
 #pragma once
+#include<vector>
+#include<string>
+#include<list>
+#include<map>
+#include<unordered_set>
+#include<sstream>
 
 namespace vsite::oop::v9 
 {
+	void fill_vector(std::vector<int>& v, int n);
+	void remove_element(std::vector<int>& v, int n);
+	void input_element(std::vector<std::string>& s, int n, std::string str);
+	int list_nth_element(std::list<int>& l, int n);
+	void list_sort_desc(std::list<int>& l);
+	unsigned int unique_numbers(std::istream& is);
+	
+	class word_frequency {
+		std::map<std::string, unsigned int> map;
+	public:
+		word_frequency(std::stringstream& ss);
+		unsigned int count() const;
+		unsigned int frequency(std::string s) const;
+	};
+
+
 }
