179fc5e4d035735a0de3134030c30b4d5ad5293e
Đuro Rukavina <djrukavina@vsite.hr>
2024-01-20 12:53:56 +0100

    V9 resolved

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,43 @@
 
 namespace vsite::oop::v9
 {
-
+	void fill_vector(std::vector<int>& v, int size) {
+		for (int i = 0; i < size; ++i) {
+			v.push_back(i * i);
+		}
+	}
+	void remove_element(std::vector<int>& v, int position) {
+		v.erase(v.begin() + position);
+	}
+	void input_element(std::vector<std::string>& v, int position, std::string s) {
+		v.insert(v.begin() + position, s);
+	}
+	int list_nth_element(std::list<int>& l, int position) {
+		std::list<int>::iterator it = l.begin();
+		std::advance(it, position);
+		return *it;
+	}
+	void list_sort_desc(std::list<int>& l) {
+		l.sort(std::greater<int>());
+	}
+	unsigned int unique_numbers(std::istream& is) {
+		std::set<int> s;
+		int number;
+		while (is >> number) s.insert(number);
+		return s.size();
+	}
+	word_frequency::word_frequency(std::istream& is) {
+		std::string word;
+		while (is >> word) {
+			for (char& c : word) c = tolower(c);
+			words[word]++;
+		}
+	}
+	unsigned int word_frequency::count() const {
+		return words.size();
+	}
+	unsigned int word_frequency::frequency(const std::string& word) const {
+		auto it = words.find(word);
+		return it != words.end() ? it->second : 0;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <set>
+#include <iostream>
 
 namespace vsite::oop::v9 
 {
+	void fill_vector(std::vector<int>& v, int size);
+	void remove_element(std::vector<int>& v, int position);
+	void input_element(std::vector<std::string>& v, int position, std::string s);
+	int list_nth_element(std::list<int>& l, int position);
+	void list_sort_desc(std::list<int>& l);
+	unsigned int unique_numbers(std::istream& is);
+
+	class word_frequency {
+		std::map<std::string, unsigned int> words;
+	public:
+		word_frequency(std::istream& is);
+		unsigned int count() const;
+		unsigned int frequency(const std::string& word) const;
+	};
 }
